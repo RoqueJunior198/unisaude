ea03b120c93b45b8a22048f672916fb169eac7d4
RoqueJunior198 <roque.junior@criciuma.unimedsc.com.br>
2026-03-16 10:27:21 -0300

    atualização html

---  exercicios-roque/dia2/CalculadoraDesconto.php
+++  exercicios-roque/dia2/CalculadoraDesconto.php
@@ -22,6 +22,8 @@ if (isset($_POST["limpar"])) {
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calculadora de Desconto</title>
 
     <style>
